0bd42b99e4897847cebf3fe70d1864f796eb32fa
pichik <pichik1337@gmail.com>
2024-11-10 19:14:38 +0100

    ye

--- tool/go.mod
+++ tool/go.mod
@@ -2,4 +2,3 @@ module github.com/pichik/go-modules/tool
 
 go 1.23.2
 
-require github.com/pichik/go-modules/output v0.0.0-20241110115903-d369fb5d1bbb
--- tool/help.go
+++ tool/help.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/pichik/go-modules/output"
+	"github.com/pichik/go-modules/misc"
 )
 
 func PrintLogo() {
-	fmt.Printf("%s%s%s\n", output.Purple, logo(), output.White)
+	fmt.Printf("%s%s%s\n", misc.Purple, logo(), misc.White)
 }
 
 func PrintToolName(name string) {
-	fmt.Printf("%s%s%s\n", output.Red, name, output.White)
+	fmt.Printf("%s%s%s\n", misc.Red, name, misc.White)
 }
 
 func PrintDefaultHelp() {
@@ -33,7 +33,7 @@ func PrintDefaultHelp() {
 	   %scat urls.txt | thetool [tool] [-h]%s
 	 Make request from string:
 	   %secho 'google.com' | thetool [tool] [-h]%s
-  `, output.Yellow, output.White, tools, output.Yellow, output.White, output.Blue, output.White, output.Blue, output.White)
+  `, misc.Yellow, misc.White, tools, misc.Yellow, misc.White, misc.Blue, misc.White, misc.Blue, misc.White)
 	os.Exit(0)
 }
 
@@ -42,16 +42,16 @@ func PrintToolHelp(toolData ToolData) {
 
 	//INCLUDED UTILS INFO
 	for _, util := range toolData.Utils {
-		fmt.Printf("%s%s\n%s", output.Green, util.Name, output.White)
+		fmt.Printf("%s%s\n%s", misc.Green, util.Name, misc.White)
 
 		//Print Flags
 		if len(util.FlagDatas) > 0 {
 			if util.Name != "" {
-				fmt.Printf(" %sFlags:\n%s", output.Yellow, output.White)
+				fmt.Printf(" %sFlags:\n%s", misc.Yellow, misc.White)
 			}
 			for _, flag := range util.FlagDatas {
 				if flag.Def != "" {
-					flag.Def = fmt.Sprintf("%s\n\t\t(Default: %v)%s", output.Gray, flag.Def, output.White)
+					flag.Def = fmt.Sprintf("%s\n\t\t(Default: %v)%s", misc.Gray, flag.Def, misc.White)
 				}
 				fmt.Printf("\t-%s%s\t%s%s\n", flag.Name, required(flag.Required), flag.Description, flag.Def)
 			}
@@ -62,11 +62,11 @@ func PrintToolHelp(toolData ToolData) {
 
 	//MAIN TOOL INFO
 	if len(toolData.FlagDatas) > 0 {
-		fmt.Printf(" %sFlags:\n%s", output.Yellow, output.White)
+		fmt.Printf(" %sFlags:\n%s", misc.Yellow, misc.White)
 
 		for _, flag := range toolData.FlagDatas {
 			if flag.Def != "" {
-				flag.Def = fmt.Sprintf("%s\n\t\t(Default: %v)%s", output.Gray, flag.Def, output.White)
+				flag.Def = fmt.Sprintf("%s\n\t\t(Default: %v)%s", misc.Gray, flag.Def, misc.White)
 			}
 			fmt.Printf("\t-%s%s\t%s%s\n", flag.Name, required(flag.Required), flag.Description, flag.Def)
 		}
@@ -80,10 +80,10 @@ func PrintToolHelp(toolData ToolData) {
 func printExamples(examples map[string]string) {
 
 	if len(examples) > 0 {
-		fmt.Printf(" %sExamples:\n%s", output.Yellow, output.White)
+		fmt.Printf(" %sExamples:\n%s", misc.Yellow, misc.White)
 
 		for description, example := range examples {
-			fmt.Printf("\t%s:\n\t %s%s%s\n", description, output.Blue, example, output.White)
+			fmt.Printf("\t%s:\n\t %s%s%s\n", description, misc.Blue, example, misc.White)
 		}
 	}
 }
@@ -103,7 +103,7 @@ func logo() string {
 
 func required(is bool) string {
 	if is {
-		return fmt.Sprintf("%s%s%s", output.Red, "!R", output.White)
+		return fmt.Sprintf("%s%s%s", misc.Red, "!R", misc.White)
 	}
 	return ""
 }
